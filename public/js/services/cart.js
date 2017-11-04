34867dba42aa4427e2a73f9fd80d04142719d2d9
Christopher Kang <christopher.kang@sjsu.edu>
2017-11-03 19:37:36 -0700

    updated order review in checkout

--- public/js/services/cart.js
+++ public/js/services/cart.js
@@ -9,39 +9,39 @@ angular.module('MyApp')
       }
     }
   })
-  // .service('Checkout', function() {
-  //   var service = {
-  //     address: null,
-  //     cartInfo: null,
-  //     delivery: 'drone',
-  //     payment: 'stripe'
-  //   }
-  //
-  //   service.getAddress = function () {
-  //     return this.address
-  //   }
-  //   service.getDelivery = function() {
-  //     return this.delivery
-  //   }
-  //   service.getCartInfo = function () {
-  //     return this.cartInfo
-  //   }
-  //   service.getPayment = function() {
-  //     return this.payment
-  //   }
-  //
-  //   service.setAddress = function(data) {
-  //     this.address = data
-  //   }
-  //   service.setDelivery = function(data) {
-  //     this.delivery = data
-  //   }
-  //   service.setCartInfo = function(data) {
-  //     this.cartInfo = data
-  //   }
-  //   service.setPayment = function(data) {
-  //     this.payment = data
-  //   }
-  //
-  //   return service
-  // })
+  .service('Checkout', function() {
+    var service = {
+      address: null,
+      cartInfo: null,
+      delivery: 'drone',
+      payment: 'stripe'
+    }
+
+    service.getAddress = function () {
+      return this.address
+    }
+    service.getDelivery = function() {
+      return this.delivery
+    }
+    service.getCartInfo = function () {
+      return this.cartInfo
+    }
+    service.getPayment = function() {
+      return this.payment
+    }
+
+    service.setAddress = function(data) {
+      this.address = data
+    }
+    service.setDelivery = function(data) {
+      this.delivery = data
+    }
+    service.setCartInfo = function(data) {
+      this.cartInfo = data
+    }
+    service.setPayment = function(data) {
+      this.payment = data
+    }
+
+    return service
+  })
