063d09350511248856450e0c341f4d6a2ea73104
Adam Homann <adamhomann@Adams-MacBook-Pro.local>
2017-12-07 18:08:04 -0800

    "Calculates time and sends to database"

--- public/js/controllers/checkout.js
+++ public/js/controllers/checkout.js
@@ -125,22 +125,139 @@ angular.module('MyApp')
     };
 
     $scope.placeOrder = function() {
-      var data = localStorageService.get('chargeData')
-      data['delivery_date'] = localStorageService.get('delivery_date')
-      console.log(data)
-      Cart.placeOrder(data)
-        .then(function(response) {
-          localStorageService.remove('address')
-          localStorageService.remove('delivery')
-          localStorageService.remove('chargeData')
-          localStorageService.remove('cartInfo')
-          $location.path('my-orders')
-        })
-        .catch(function(response) {
-          $scope.messages = {
-            error: Array.isArray(response.data) ? response.data : [response.data]
-          };
-        })
+        eta()
     }
+
+
+
+
+
+    $scope.var = $rootScope.currentUser;
+    $scope.googleMapRequest = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAd3Q644s4HHXat5mvN8xKlyT7pi1A3eYY&callback=initMap"
+    // $scope.googleMapRequest = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg/1200px-Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg";
+    var distanceTotal, now, currentTimeStamp, orderDate, elapsedTime, distanceTotalInMiles, msToTravelDistance, ratioTripSoFar, startLocation;
+    var isSameDay = function(){
+      now = new Date(); // returned PST
+      currentTimeStamp = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+      orderDate = new Date(localStorageService.get('trackingInfo').order_date)
+      // alert($scope.order)
+      // console.log(now, currentTimeStamp, "hi", orderDate);
+      elapsedTime = now.getTime() - orderDate.getTime() - 70000
+
+    };
+
+    //
+    //
+    // $scope.order_time = localStorageService.get('trackingInfo').order_date).substring()
+    $scope.address = localStorageService.get('address')//('trackingInfo').address//"39831 San Moreno Ct Fremont CA" //localStorageService.get('trackingInfo').address
+    // JavaScript source code
+
+
+    var startLoc = new Array();
+    startLoc[0] =  { lat: 37.554260, lng: -122.308524 };
+    startLoc[1] = { lat: 37.350531, lng: -121.922172 };
+
+    var endLoc = new Array();
+    var finalDestination;
+    endLoc[0];
+    endLoc[1];
+
+
+
+    function eta(){
+      var time_of_delivery = 0;
+      finalDestination = $scope.address
+
+      endLoc[0] = finalDestination;
+      endLoc[1] = finalDestination;
+
+      address = finalDestination
+      geocoder = new google.maps.Geocoder();
+      geocoder.geocode( { 'address': address}, function(results, status) {
+        var markerC = new google.maps.Marker({
+         position: results[0].geometry.location,
+         icon:'/pics/house.png',
+         title:finalDestination
+        });
+      });
+
+        var distCalcService = new google.maps.DistanceMatrixService;
+        distCalcService.getDistanceMatrix({
+            origins: [startLoc[0], startLoc[1]],
+            destinations: [$scope.address, $scope.address],
+            travelMode: 'DRIVING',
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false},function(response, status)
+            {
+              if (status !== 'OK') {
+                  alert('Error was: ' + status);
+              } else {
+                var distSM = response.rows[0].elements[0].distance.value;
+                var distSJ = response.rows[1].elements[0].distance.value;
+
+
+
+                var travelMode = google.maps.DirectionsTravelMode.DRIVING;
+
+                directionsService = new google.maps.DirectionsService();
+
+                // If San Jose is closer
+                if(distSM >distSJ)
+                {
+                  distanceTotal = distSJ
+                }
+                // If San Mateo is closer
+
+                else
+                {
+                  distanceTotal = distSM
+                }
+                distanceTotalInMiles = distanceTotal / 1609.34
+                msToTravelDistance = distanceTotalInMiles * 60000
+                time_of_delivery = new Date(Date.now() + msToTravelDistance)
+
+
+
+                var data = localStorageService.get('chargeData')
+                data['delivery_date'] = time_of_delivery
+                console.log(data)
+                Cart.placeOrder(data)
+                  .then(function(response) {
+                    localStorageService.remove('address')
+                    localStorageService.remove('delivery')
+                    localStorageService.remove('chargeData')
+                    localStorageService.remove('cartInfo')
+                    $location.path('my-orders')
+                  })
+                  .catch(function(response) {
+                    $scope.messages = {
+                      error: Array.isArray(response.data) ? response.data : [response.data]
+                    };
+                  })
+
+
+              }});
+    }
+
+
+$.getScript($scope.googleMapRequest, function() {
+
+// alert("Script loaded but not necessarily executed.");
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   });
